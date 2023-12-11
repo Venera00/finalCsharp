854014b69cb70f069ef0ea4884667740ff152a56
Venera <veneraomurzakkyzy@gmail.com>
2023-12-11 13:30:42 +0600

    Добавьте файлы проекта.

---
+++ final/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace YourNamespace
+{
+    partial class TaskManagerForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxTasks = new System.Windows.Forms.ListBox();
+            this.taskNameTextBox = new System.Windows.Forms.TextBox();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.deadlineDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.addButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxTasks
+            // 
+            this.listBoxTasks.ItemHeight = 16;
+            this.listBoxTasks.Location = new System.Drawing.Point(20, 20);
+            this.listBoxTasks.Name = "listBoxTasks";
+            this.listBoxTasks.Size = new System.Drawing.Size(200, 292);
+            this.listBoxTasks.TabIndex = 0;
+            // 
+            // taskNameTextBox
+            // 
+            this.taskNameTextBox.Location = new System.Drawing.Point(250, 20);
+            this.taskNameTextBox.Name = "taskNameTextBox";
+            this.taskNameTextBox.Size = new System.Drawing.Size(200, 22);
+            this.taskNameTextBox.TabIndex = 1;
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(250, 60);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(200, 22);
+            this.descriptionTextBox.TabIndex = 2;
+            // 
+            // deadlineDateTimePicker
+            // 
+            this.deadlineDateTimePicker.Location = new System.Drawing.Point(250, 180);
+            this.deadlineDateTimePicker.Name = "deadlineDateTimePicker";
+            this.deadlineDateTimePicker.Size = new System.Drawing.Size(200, 22);
+            this.deadlineDateTimePicker.TabIndex = 3;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(250, 220);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 30);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(330, 220);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(75, 30);
+            this.editButton.TabIndex = 5;
+            this.editButton.Text = "Edit";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(410, 220);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 30);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "Delete";
+            // 
+            // TaskManagerForm
+            // 
+            this.ClientSize = new System.Drawing.Size(758, 428);
+            this.Controls.Add(this.listBoxTasks);
+            this.Controls.Add(this.taskNameTextBox);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.deadlineDateTimePicker);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.deleteButton);
+            this.Name = "TaskManagerForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ final/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace YourNamespace
+{
+    public partial class TaskManagerForm : Form
+    {
+        private ListBox listBoxTasks;
+        private TextBox taskNameTextBox;
+        private TextBox descriptionTextBox;
+        private DateTimePicker deadlineDateTimePicker;
+        private Button addButton;
+        private Button editButton;
+        private Button deleteButton;
+
+        public TaskManagerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            
+
+            // Example: Initialize controls with their properties and add them to the form
+            listBoxTasks = new ListBox();
+            listBoxTasks.Location = new System.Drawing.Point(20, 20);
+            listBoxTasks.Size = new System.Drawing.Size(200, 300);
+            Controls.Add(listBoxTasks);
+
+            taskNameTextBox = new TextBox();
+            taskNameTextBox.Location = new System.Drawing.Point(250, 20);
+            taskNameTextBox.Size = new System.Drawing.Size(200, 20);
+            Controls.Add(taskNameTextBox);
+
+            descriptionTextBox = new TextBox();
+            descriptionTextBox.Location = new System.Drawing.Point(250, 60);
+            descriptionTextBox.Size = new System.Drawing.Size(200, 100);
+            Controls.Add(descriptionTextBox);
+
+            deadlineDateTimePicker = new DateTimePicker();
+            deadlineDateTimePicker.Location = new System.Drawing.Point(250, 180);
+            deadlineDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            Controls.Add(deadlineDateTimePicker);
+
+            addButton = new Button();
+            addButton.Text = "Add";
+            addButton.Location = new System.Drawing.Point(250, 220);
+            addButton.Size = new System.Drawing.Size(75, 30);
+            addButton.Click += addButton_Click;
+            Controls.Add(addButton);
+
+            editButton = new Button();
+            editButton.Text = "Edit";
+            editButton.Location = new System.Drawing.Point(330, 220);
+            editButton.Size = new System.Drawing.Size(75, 30);
+            editButton.Click += editButton_Click;
+            Controls.Add(editButton);
+
+            deleteButton = new Button();
+            deleteButton.Text = "Delete";
+            deleteButton.Location = new System.Drawing.Point(410, 220);
+            deleteButton.Size = new System.Drawing.Size(75, 30);
+            deleteButton.Click += deleteButton_Click;
+            Controls.Add(deleteButton);
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string taskName = taskNameTextBox.Text;
+            string description = descriptionTextBox.Text;
+            DateTime deadline = deadlineDateTimePicker.Value;
+
+            
+            listBoxTasks.Items.Add($"{taskName} - Due: {deadline.ToShortDateString()}");
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.SelectedIndex >= 0)
+            {
+                string taskName = taskNameTextBox.Text;
+                string description = descriptionTextBox.Text;
+                DateTime deadline = deadlineDateTimePicker.Value;
+
+                // Update the selected task in the list
+                listBoxTasks.Items[listBoxTasks.SelectedIndex] = $"{taskName} - Due: {deadline.ToShortDateString()}";
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.SelectedIndex >= 0)
+            {
+                // Delete the selected task from the list
+                listBoxTasks.Items.RemoveAt(listBoxTasks.SelectedIndex);
+            }
+        }
+
+        private void taskNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
